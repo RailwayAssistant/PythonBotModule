f238c31b86461385407c4713eb68793733435bdd
Shashank Kumar <abc@shanky.xyz>
2018-04-19 01:05:46 +0530

    New features added to bot
    New features are
    - Train live status
    - Cancelled Trains
    - Reschedules Trains

--- server.py
+++ server.py
@@ -9,14 +9,22 @@ application = Flask(__name__)
 
 API_KEY = os.environ.get('RAAPIKEY')
 
+# Sample Dates
+YDate = '18-04-2018'
+TDate = '19-04-2018'
+TMDate = '20-04-2018'
+
 def related_to_railway(response):
     response = response.lower()
-    if 'pnr' in response:
-        PNR = ''
-        for x in response.split():
-            if x.isnumeric() and len(x) == 10:
-                PNR = x
-                break
+    if ('pnr' in response) or (len(response)==10 and response.isdigit()):
+        if len(response) != 10:
+            PNR = ''
+            for x in response.split():
+                if x.isnumeric() and len(x) == 10:
+                    PNR = x
+                    break
+        else:
+            PNR = response
         if PNR:
             url = 'https://api.railwayapi.com/v2/pnr-status/pnr/{}/apikey/{}/'.format(PNR, API_KEY)
             response =  requests.get(url)
@@ -41,6 +49,96 @@ def related_to_railway(response):
 
         else:
             return 'Please provide a valid PNR also with your question.'
+    elif 'cancelled' in response or response[0:2] == 'c/':
+        if response[0:2] != 'c/':
+            if 'yesterday' in response:
+                DATE = YDate
+            elif 'today' in response:
+                DATE = TDate
+            elif 'tomorrow' in response:
+                DATE = TMDate
+            else:
+                return 'Please ask for either Yesterday, Today or Tommorow.'
+        else:
+            DATE = response[2:]
+        url = 'https://api.railwayapi.com/v2/cancelled/date/{}/apikey/{}/'.format(DATE, API_KEY)
+        response =  requests.get(url)
+        if response.status_code != 200:
+            return 'Cannot contact my guys in railway, please remind me later.'
+        else:
+            try:
+                response = response.json()
+                details_to_return = []
+                details_to_return.append("On " + DATE + " a total of " + str(response["total"]) + " trains were cancelled.")
+                if response["total"]:
+                    details_to_return.append("They were.")
+                    for x in response["trains"]:
+                        details_to_return.append(x["name"] + ", " + x["type"]+ " train, going from " + x["source"]["name"] + " to " + x["dest"]["name"] + ".")
+                    return ' '.join(details_to_return)
+            except Exception as e:
+                application.logger.error(e)
+                return 'Railway guys are not listening to me, maybe they are busy somewhere. Please try after lunch.'
+    elif 'rescheduled' in response or response[0:2] == 'r/':
+        if response[0:2] != 'r/':
+            if 'yesterday' in response:
+                DATE = YDate
+            elif 'today' in response:
+                DATE = TDate
+            elif 'tomorrow' in response:
+                DATE = TMDate
+            else:
+                return 'Please ask for either Yesterday, Today or Tommorow.'
+        else:
+            DATE = response[2:]
+        url = 'https://api.railwayapi.com/v2/rescheduled/date/{}/apikey/{}/'.format(DATE, API_KEY)
+        response =  requests.get(url)
+        if response.status_code != 200:
+            return 'Cannot contact my guys in railway, please remind me later.'
+        else:
+            try:
+                response = response.json()
+                details_to_return = []
+                details_to_return.append("On " + DATE + " these trains were rescheduled.")
+                for x in response["trains"]:
+                    details_to_return.append(x["name"] + " train, going from " + x["from_station"]["name"] + " to " + x["to_station"]["name"] + " is now rescheduled to " + x["rescheduled_date"] + " at " + x["rescheduled_time"] + ".")
+                return ' '.join(details_to_return)
+            except Exception as e:
+                application.logger.error(e)
+                return 'Railway guys are not listening to me, maybe they are busy somewhere. Please try after lunch.'
+    elif ('live' in response and 'status' in response) or (response.count('/')==2):
+        if response.count("/")!=2:
+            if 'yesterday' in response:
+                DATE = YDate
+            elif 'today' in response:
+                DATE = TDate
+            elif 'tomorrow' in response:
+                DATE = TMDate
+            else:
+                return 'Please ask for either Yesterday, Today or Tommorow.'
+            TRAIN_NO = ''
+            for x in response.split():
+                if x.isnumeric() and len(x) == 5:
+                    TRAIN_NO = x
+                    break
+        else:
+            response = response.split('/')
+            TRAIN_NO = response[0]
+            DATE = response[2]
+        url = 'https://api.railwayapi.com/v2/live/train/{}/date/{}/apikey/{}/'.format(TRAIN_NO, DATE, API_KEY)
+        response =  requests.get(url)
+        if response.status_code != 200:
+            return 'Cannot contact my guys in railway, please remind me later.'
+        else:
+            try:
+                response = response.json()
+                details_to_return = []
+                details_to_return.append(response["train"]["number"] + ", " + response["train"]["name"] + ".")
+                details_to_return.append(response["position"])
+                return ' '.join(details_to_return)
+            except Exception as e:
+                application.logger.error(e)
+                return 'Railway guys are not listening to me, maybe they are busy somewhere. Please try after lunch.'
+
     return False
 
 def get_appropriate_response(response):
