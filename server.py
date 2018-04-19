7eb3ce4287e6cb92fc7c2806d9864fe4306c8e55
Shashank Kumar <abc@shanky.xyz>
2018-04-19 14:37:45 +0530

    Few updated and bug fixes

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import os
 
 application = Flask(__name__)
 
-API_KEY = os.environ.get('RAAPIKEY')
+#API_KEY = os.environ.get('RAAPIKEY')
 
 # Sample Dates
 YDate = '18-04-2018'
@@ -16,15 +16,16 @@ TMDate = '20-04-2018'
 
 def related_to_railway(response):
     response = response.lower()
-    if ('pnr' in response) or (len(response)==10 and response.isdigit()):
-        if len(response) != 10:
+    if 'pnr' in response:
+        if response[0:4] != 'pnr/':
             PNR = ''
             for x in response.split():
                 if x.isnumeric() and len(x) == 10:
                     PNR = x
+                    print(x)
                     break
         else:
-            PNR = response
+            PNR = response[4:]
         if PNR:
             url = 'https://api.railwayapi.com/v2/pnr-status/pnr/{}/apikey/{}/'.format(PNR, API_KEY)
             response =  requests.get(url)
@@ -33,16 +34,18 @@ def related_to_railway(response):
             else:
                 try:
                     response = response.json()
-                    details_to_return = []
-                    passengers = response['passengers']
-                    details_to_return.append("Here I have details of " + str(len(passengers)) + " passenger{}".format("s." if (len(passengers) - 1) else "."))
-                    for x in passengers:
-                        details_to_return.append("Passenger no "+ str(x["no"]) + "'s current status is "+ x['current_status'] + ", it was "+ x['booking_status'] + " at the time of booking.")
-                    from_station = response["from_station"]["name"]
-                    to_station = response["to_station"]["name"]
-                    details_to_return.append("Train the PNR refers to is "+ response["train"]["name"] + ". It'll run from "+ response["from_station"]["name"] + " to " + response["to_station"]["name"])
-                    details_to_return.append("Make sure you board on "+ response["boarding_point"]["name"] + " on " + response["doj"]+ ".")
-                    return ' '.join(details_to_return)
+                    print(response)
+                    if response['response_code'] == 200:
+                        details_to_return = []
+                        passengers = response['passengers']
+                        details_to_return.append("Here I have details of " + str(len(passengers)) + " passenger{}".format("s." if (len(passengers) - 1) else "."))
+                        for x in passengers:
+                            details_to_return.append("Passenger no "+ str(x["no"]) + "'s current status is "+ x['current_status'] + ", it was "+ x['booking_status'] + " at the time of booking.")
+                        details_to_return.append("Train the PNR refers to is "+ response["train"]["name"] + ". It'll run from "+ response["from_station"]["name"] + " to " + response["to_station"]["name"])
+                        details_to_return.append("Make sure you board on "+ response["boarding_point"]["name"] + " on " + response["doj"]+ ".")
+                        return ' '.join(details_to_return)
+                    else:
+                        return 'Please provide a valid PNR also with your question.'
                 except Exception as e:
                     application.logger.error(e)
                     return 'Please provide a valid PNR also with your question.'
@@ -155,7 +158,10 @@ def hello():
     except:
         response = None
     if response:
-        return get_appropriate_response(response)
+        try:
+            return get_appropriate_response(response)
+        except:
+            return 'Damn it! Railway guys are not reachable!'
     else:
         return 'Cannot understand that, maybe I need more training'
 
